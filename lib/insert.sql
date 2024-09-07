3527b4ab5dd31c5d19ad119dec902938d0a3bac3
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-09-07 18:42:44 +0300

    p3-organizing bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Yogi', 10, 'M', 'Brown', 'Friendly', 1),
+('Paddington', 5, 'M', 'Brown', 'Curious', 1),
+('Teddy', 6, 'M', 'Black', 'Aggressive', 0),
+(NULL, 7, 'F', 'Black', 'Calm', 1),
+('Baloo', 15, 'M', 'Grey', 'Relaxed', 1),
+('Winnie', 8, 'M', 'Yellow', 'Cheerful', 1),
+('Smokey', 20, 'F', 'Brown', 'Protective', 0),
+('Grizzly', 13, 'M', 'Brown', 'Wild', 1);
--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,11 @@
-INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sergeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'Black', 'Calm', 0),
+('Rowdy', 10, 'M', 'Brown', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'Brown', 'Friendly', 1),
+('Grinch', 2, 'M', 'Black', 'Grumpy', 1),
+('Sergeant Brown', 9, 'M', 'Brown', 'Calm', 0),
+('Melissa', 13, 'F', 'Black', 'Cheerful', 1),
+('Wendy', 6, 'F', 'Brown', 'Aggressive', 1),
+(NULL, 7, 'M', 'Gray', 'Unknown', 0);
+
+SELECT name, age FROM bears WHERE sex = 'F';
